6c84516a1ffeb20d6a9012727cc36d463bf9e2f4
Mike McCready <mikem@inspero.net>
2016-10-20 23:59:24 -0700

    adding comments to comment list, not persistant

--- plugins/comments/Comment.js
+++ plugins/comments/Comment.js
@@ -5,22 +5,45 @@ class Comment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	comments: []
+    	comments: [],
+    	input: ''
     }
   }
 
   componentDidMount() {}
 
+  updateInput(e) {
+  	let newInput = e.target.value;
+  	this.setState({input: newInput});
+  }
+
+  addComment() {
+  	let comment = this.state.input;
+  	let commentList = this.state.comments.slice();
+  	commentList.push(comment);
+  	this.setState({comments: commentList})
+  	this.clearInput();
+  }
+
+  clearInput() {
+  	let input = '';
+  	this.refs.commentInput.value = input;
+  	this.setState({input: input});
+  }
+
   render() {
 
-
+  	let commentList = this.state.comments.map((comment, i) => {
+  		return <div key={i}>{comment}</div>
+  	})
   	
     return (
     	<div>
     		Comments
-    		<input placeholder="leave a comment" />
+    		<input ref="commentInput" placeholder="leave a comment" onChange={(e) => {this.updateInput(e)}} />
+    		<button onClick={(e) => {this.addComment(e)}}>Enter</button>
     		<div className="comment-list">
-
+    			{commentList}
     		</div>
     	</div>
     )
