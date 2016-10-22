6edd9c9647735b8d7459aef95b3d6cbdcef96e9c
Todd Chisom <hello@toddchisom.me>
2016-10-22 16:50:19 -0700

    add post to reducer

--- admin/lib/reducers/rootReducer.js
+++ admin/lib/reducers/rootReducer.js
@@ -4,8 +4,9 @@ import { routerReducer } from 'react-router-redux';
 import user from './userReducer';
 import components from './componentsReducer';
 import posts from './posts';
+import post from '../post';
 
-const rootReducer = combineReducers({user, components, posts, routing: routerReducer});
+const rootReducer = combineReducers({user, components, posts, [post.constants.NAME] : post,  routing: routerReducer});
 
 export default rootReducer;
 
