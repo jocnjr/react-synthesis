6636ad161721771f1a80c77965d69d121ec823ab
Mike McCready <mikem@inspero.net>
2016-10-24 12:54:08 -0700

    converting comment plugin structure

--- plugins/index.js
+++ plugins/index.js
@@ -3,12 +3,12 @@
 const Collection = require('./collection/containers/Data');
 const PostManager = require('./post-manager/containers/Posts');
 const PostFeed = require('./post/containers/PostFeed');
-const Comment = require('./comments/Comment');
+const Comments = require('./Comments/components/Comments');
 
 // import plugin config
 const CollectionData = require('./collection/plugin.synth');
 const PostManagerData = require('./post-manager/plugin.synth');
 const PostFeedData = require('./post/plugin.synth');
-const CommentData = require('./comments/plugin.synth');
+const CommentsData = require('./Comments/plugin.synth');
 
-module.exports = { Collection, CollectionData, PostManager, PostManagerData, PostFeed, PostFeedData, Comment, CommentData }
+module.exports = { Collection, CollectionData, PostManager, PostManagerData, PostFeed, PostFeedData, Comments, CommentsData }
