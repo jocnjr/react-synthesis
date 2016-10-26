f88b43afd19f73090416769987637b4cfcb7f6be
Jose Neves <joc.neves@gmail.com>
2016-10-26 11:57:33 -0700

    correcting plugins indexjs to correct routes and serverjs update to get ready for deployment

--- plugins/index.js
+++ plugins/index.js
@@ -1,12 +1,12 @@
 // index.js
 // gather components in plugin folder for export
 const DataManager = require('./DataManager/containers/DataManager');
-const PostManager = require('./post-manager/containers/Posts');
+const PostManager = require('./PostManager/containers/PostManager');
 const Comments = require('./Comments/components/Comments');
 
 // import plugin config
 const DataManagerData = require('./DataManager/plugin.synth');
-const PostManagerData = require('./post-manager/plugin.synth');
+const PostManagerData = require('./PostManager/plugin.synth');
 const CommentsData = require('./Comments/plugin.synth');
 
 module.exports = { DataManager, DataManagerData, PostManager, PostManagerData, Comments, CommentsData }
