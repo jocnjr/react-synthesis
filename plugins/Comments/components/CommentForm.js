4b7d1800ecbac15590c8fc9f3b35503aa9898747
Mike McCready <mikem@inspero.net>
2016-11-01 16:08:47 -0700

    add post view styling

--- plugins/Comments/components/CommentForm.js
+++ plugins/Comments/components/CommentForm.js
@@ -5,14 +5,14 @@ export default function CommentForm(props) {
     return(
     	<div className="form-group">
     		<input className="form-control comment-box" 
-    			placeholder="leave your name" 
+    			placeholder="Your name" 
     			onChange={(e) => {props.updateInput(e, "author")}} 
     			value={props.author} />
-        <input className="form-control comment-box" 
-        	placeholder="leave a comment" 
+        <textarea className="form-control comment-box" 
+        	placeholder="Say something" 
         	onChange={(e) => {props.updateInput(e, "comment")}} 
-        	value={props.comment} />          
-    		<button className="btn btn-default" onClick={(e) => {props.addComment(e)}}>Enter</button>
+        	value={props.comment}  />          
+    		<button className="btn btn-default comment-submit" onClick={(e) => {props.addComment(e)}}>Enter</button>
     	</div>
     );
 };
