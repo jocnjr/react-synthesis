8bced48e24f9616fd384cfe798a8f0184c01c18c
Mike McCready <mikem@inspero.net>
2016-11-02 00:20:36 -0700

    format post view for demo

--- admin/lib/leadHero/components/LeadHero.js
+++ admin/lib/leadHero/components/LeadHero.js
@@ -2,32 +2,35 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
+export default function Signup(props) {
+	let featuredKey = Object.keys(props.postData)[0];
+	let featured = props.postData[featuredKey];
+	let featuredTitle = '';
+	let postUrl;
+  let ctaButton;
+  let heroStyle = {};
+  let isFeatured;
 
+	if (featured) {
+		featuredTitle = featured.title;
+		postUrl = '/post/' + featured._id;
+	}
+
+  //check if home route
+  if (window.location.pathname === '/') {
+    isFeatured = <h5>Featured article</h5>
+    ctaButton = <Link to={postUrl}><button className="LeadHero-cta">Read More</button></Link>;
+  } else {
+     heroStyle.height = '15em'
   }
 
-  render() {
-  	let featuredKey = Object.keys(this.props.postData)[0];
-  	let featured = this.props.postData[featuredKey];
-  	let featuredTitle = '';
-  	let postUrl;
-
-  	if (featured) {
-  		featuredTitle = featured.title;
-  		postUrl = '/post/' + featured._id;
-  	}
-
-    return(
-    	<div className="LeadHero">
-    		<div className="LeadHeroContainer col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-	    		Featured:
-	    		<h1 className="LeadHero-title">{featuredTitle}</h1>
-	    		<Link to={postUrl}><button className="LeadHero-cta">Read More</button></Link>
-	    	</div>
+  return(
+  	<div className="LeadHero" style={heroStyle}>
+  		<div className="LeadHeroContainer col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+        {isFeatured}
+    		<h1 className="LeadHero-title">{featuredTitle}</h1>
+        {ctaButton}
     	</div>
-    )
-  }
+  	</div>
+  )
 }
