0283b98f4f1556fa715130aefe2ed72cc86eebe6
Mike McCready <mikem@inspero.net>
2016-11-01 23:32:01 -0700

    create hero image component, with display for featured post

--- admin/lib/content-page/Content.js
+++ admin/lib/content-page/Content.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import { Link } from 'react-router';
 // import components
-import * as Components from "../../../plugins";
-import PostFeed from "../post/components/PostFeed"
+import * as Components from '../../../plugins';
+import PostFeed from '../post/components/PostFeed';
+import LeadHero from '../leadHero/components/LeadHero';
 
 export default class Content extends React.Component {
   render() {
     return (
       	<div>
+      		<LeadHero postData={this.props.posts} />
     		<PostFeed postData={this.props.posts} />
     	</div>
     )
