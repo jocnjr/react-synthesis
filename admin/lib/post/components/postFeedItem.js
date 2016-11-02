7593eb7114c002d44c1a27a3b2765b56154644d8
Mike McCready <mikem@inspero.net>
2016-11-01 22:26:33 -0700

    add post preview to feed items

--- admin/lib/post/components/postFeedItem.js
+++ admin/lib/post/components/postFeedItem.js
@@ -2,13 +2,20 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default class PostFeedItem extends React.Component {
-  render() {
-  	let postUrl = '/post/' + this.props.postData._id;
+export default function PostFeedItem (props) {
+  	let postUrl = '/post/' + props.postData._id;
+
+  	let postPreview = props.postData.body.split(' ');
+  	postPreview = postPreview.splice(0,30)
+  	
+  	let postString = postPreview.join(' ');
+  	postString += '...';
+
     return (
-  		<div className="content-page-post" key={this.props.postData._id}>
-  			<h4><Link to={postUrl}>{ this.props.postData.title }</Link></h4>
+  		<div className="content-page-post" key={props.postData._id}>
+  			<h4><Link to={postUrl}>{ props.postData.title }</Link></h4>
+  			<p>{ postString }</p>
+  			<p><Link to={postUrl}>Read more</Link></p>
   		</div>
     )
-  }
 }
